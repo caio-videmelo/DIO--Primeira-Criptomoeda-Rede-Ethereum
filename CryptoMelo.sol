8b9fe0be82a9a0cfbb3e42c3788bb4e96b77b872
Caio Melo <caio.videmelo@gmail.com>
2024-07-23 14:21:28 -0300

    Update CryptoMelo.sol

--- CryptoMelo.sol
+++ CryptoMelo.sol
@@ -18,25 +18,29 @@ interface IERC20{
 }
 
 contract CryptoMelo is IERC20{
-	string public constant name = "Crypto_Melo";
-    	string public constant symbol = "MELO";
-    	uint8 public constant decimals = 18;    
 
-	// Mapping balance
-	mapping (address => uint256) public balanceOf;;
-   	mapping(address => mapping(address=>uint256)) allowed;
+    string public constant name = "Crypto Melo";
+    string public constant symbol = "Melo";
+    uint8 public constant decimals = 18;
 
-	// Number of coins
-    	uint256 public totalSupply = 10 ether;
-	constructor(){
+    mapping (address => uint256) balances;
+
+    mapping(address => mapping(address=>uint256)) allowed;
+
+    uint256 totalSupply_ = 10 ether;
+
+    constructor(){
         balances[msg.sender] = totalSupply_;
- }
- 
-   // Event that notifies when a transfer has been done
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    }
 
+    function totalSupply() public override view returns (uint256) {
+        return totalSupply_;
+    }
 
-    // Function to transfer coins
+    function balanceOf(address tokenOwner) public override view returns (uint256){
+        return balances[tokenOwner];
+    }
+
     function transfer(address receiver, uint256 numTokens) public override returns (bool) {
         require(numTokens <= balances[msg.sender]);
         balances[msg.sender] = balances[msg.sender]-numTokens;
@@ -44,7 +48,8 @@ contract CryptoMelo is IERC20{
         emit Transfer(msg.sender, receiver, numTokens);
         return true;
     }
-function approve(address delegate, uint256 numTokens) public override returns (bool) {
+
+    function approve(address delegate, uint256 numTokens) public override returns (bool) {
         allowed[msg.sender][delegate] = numTokens;
         emit Approval(msg.sender, delegate, numTokens);
         return true;
@@ -65,4 +70,4 @@ function approve(address delegate, uint256 numTokens) public override returns (b
         return true;
     }
 
- }
+}
